b4091784e065659513de187b799abf2fed40ccbf
Anderson Carvalho <andersonfelipe01@live.com>
2019-08-09 10:41:03 -0300

    Implemented algorithm to run a window

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import db
 import code
 import re
 import sys
-
-
+import datetime as dt
 
 
 USAGE = """\
@@ -15,7 +14,7 @@ USAGE: code.py <filter> <field> <database-file>
   Ex: 2018-01-01T15:35:00 2018-12-31T02:45:00 2018-11-29T20:45:00 0-6 P1 <database-file>
   weekdays= 0-monday, etc.
 """
-#print(sys.argv)
+
 if __name__ == "__main__":
     try:
         expr = ' '.join(sys.argv[1:-2])
@@ -52,11 +51,6 @@ if __name__ == "__main__":
     e_hour = code.timestamp_from_time(e_date)
     #print(e_hour)
     
-    h_analyze = code.timestamp_from_datetime(d_date)
-    #print(h_analyze)
-    ###COMP_DATE -28 DAYS TO THE D_DATE (2419200 = 28 days)
-    comp_date = h_analyze - 2419200
-    
     intervaldb,errors = code.find_between_timestamps(table, s, e)
 
     # filter by weekday
@@ -65,21 +59,43 @@ if __name__ == "__main__":
     if errors != None:
         code.related_errors(errors)
     
-    #gets a list of every data 28 days ago since the "present day"
-    ans_28days = code.db_28days(table, comp_date, h_analyze)    
 
-    #gets a list completed by 0's ( disposable ), end precision that we got
-    historic_efficience = code.efficience_table(ans_28days, week_expr,week_pattern)  
+    h_analyze = dt.datetime( int(d_date.split("-")[0]), int(d_date.split("-")[1]), int(d_date.split("-")[2][:2]) )
+    #print(h_analyze)
+    ###COMP_DATE -28 DAYS TO THE D_DATE (2419200 = 28 days)
+    comp_date = h_analyze
+    comp_date = h_analyze - dt.timedelta( days=28 )
 
-    #Get arithimetic mean to the present day and precision of data that we got
-    mean_day, efficience_day, ans_day = code.mean_day(table,d_date,field_number) 
+    day_1 = []
+    new_date = 0
+    while new_date <= code.timestamp_from_datetime( e_date ):
+        new_date = dt.datetime.timestamp(h_analyze)
+        new_date_str = str( h_analyze ).split()
+        new_date_str = 'T'.join(new_date_str)
+        new_comp_date = dt.datetime.timestamp(comp_date)
+        #gets a list of every data 28 days ago since the "present day"
+        ans_28days = code.db_28days(table, new_comp_date, new_date)
 
-    #Gets the arithimetic mean of historic data
-    historic_mean = code.db_mean( ans_28days, field_number )    
+        #gets a list completed by 0's ( disposable ), end precision that we got
+        historic_efficience = code.efficience_table(ans_28days, week_expr,week_pattern)  
+
+        #Get arithimetic mean to the present day and precision of data that we got
+        mean_day, efficience_day, ans_day = code.mean_day(table,new_date_str,field_number) 
+
+        #Gets the arithimetic mean of historic data
+        historic_mean = code.db_mean( ans_28days, field_number )    
+        
+        #Is a list of [ historic media, efficience of historic media, day media, efficience of day media ]
+        day_1 += [[historic_mean, historic_efficience, mean_day, efficience_day]]
+
+        h_analyze += dt.timedelta(days=1)
+        comp_date = h_analyze - dt.timedelta(days=28)
     
-    #Is a list of [ historic media, efficience of historic media, day media, efficience of day media ]
-    day_1 = [historic_mean, historic_efficience, mean_day, efficience_day]
-    
+    with open("plots/2018-05-12.csv", 'w') as file:
+        for entry in day_1:
+            file.write(str( entry )[1:-1]+'\n')
+        file.close()
+
     standard_deviation_day = code.db_standard_deviation(ans_day ,field_number)
 
     standard_deviation_historic = code.db_standard_deviation(ans_28days, field_number)
