f573aa9050062a9b49c33a0ea3b4f114ca49d8d1
Anderson Carvalho <andersonfelipe01@live.com>
2019-08-12 09:47:45 -0300

    Kmeans and graph improved

--- plots/k_means_oc.m
+++ plots/k_means_oc.m
@@ -1,31 +1,74 @@
 #! usr/bin/ -qf
 
+pkg load statistics
+
 printf("Started\n");
 
-function plot_kmeans(  )
-    db = csvread( "2018-05-12.csv" );
+function color = color_to_plot ( num )
+    if num == 1
+        color = [0.066, 0.105, 0.690];
+    elseif num == 2
+        color = [0.945, 0.917, 0.294];
+    elseif num == 3
+        color = [0.827, 0, 0.513];
+    elseif num == 4
+        color = [1, 0.545, 0.184];
+    elseif num == 5
+        color = [0.588, 0, 0.619];
+    elseif num == 6
+        color = [1, 0.325, 0.282];
+    elseif num == 7
+        color = [1, 0.741, 0.164];
+    else
+        color = [0.976, 0, 0.392];
+    endif
+endfunction
+
+function plot_kmeans( file, num )
+    db = csvread( file );
+    k = str2num(num);
     
-    C1 = [db( 1:end, 6 )];
-    C2 = [db( 1:end, 4 )];
+    maxM_h = max( db( 1:end, 1 ) );
+    minM_h = min( db( 1:end, 1 ) );
+    maxStd_h = max( db( 1:end, 3 ) );
+    minStd_h = min( db( 1:end, 3 ) );
+
+    C1 = [];
+    C2 = [];
+    for i=1:length(db)
+        if db(i, 4) != -1
+            C1 = [C1; db( i, 4 )];
+            C2 = [C2; db( i, 6 )];
+        endif
+    endfor
     data = [C1, C2];
 
     ## Perform clustering
-    [idx, centers] = kmeans(data, 5);
+    [idx, centers] = kmeans(data, k);
 
     ## Plot the result
+    ";K-means to data analisys;";
     figure;
-    plot (data (idx==1, 1), data (idx==1, 2), 'ro', "markerfacecolor", "r");
-    hold on;
-    plot (data (idx==2, 1), data (idx==2, 2), 'bo', "markerfacecolor", "b");
-    hold on;
-    plot (data (idx==3, 1), data (idx==3, 2), 'go', "markerfacecolor", "g");
-    hold on;
-    plot (data (idx==4, 1), data (idx==4, 2), 'mo', "markerfacecolor", "m");
-    hold on;
-    plot (data (idx==5, 1), data (idx==5, 2), 'co', "markerfacecolor", "c");
+
+    for i=1:k
+        color = color_to_plot( i );
+        plot (data (idx==i, 1), data (idx==i, 2), 'd', "color", color, "markerfacecolor", color);
+        hold on;
+    endfor
 
     plot (centers (:, 1), centers (:, 2), 'kv', 'markersize', 10, "markerfacecolor", "k");
+    hold on;
+    #plot ( [minM_h, minM_h, maxM_h, maxM_h, minM_h], [minStd_h, maxStd_h, maxStd_h, minStd_h, minStd_h], "linewidth", 2, "color", "r" );
+    rectangle('position', [minM_h, minStd_h, maxM_h - minM_h, maxStd_h - minStd_h], "facecolor", [0.905, 0.960, 0.980], "edgecolor", "b", "linewidth", 1);
+    hold on;
+
+    pause(10);
     hold off;
     
-endfunction 
-plot_kmeans(  );
+endfunction
+
+arg_list = argv ();   #receive the arguments in command line
+archive = strjoin( arg_list( 1, 1 ) );
+num = strjoin( arg_list( 2, 1 ) );
+
+plot_kmeans( archive, num );
--- plots/oc_plot28.m
+++ plots/oc_plot28.m
@@ -2,11 +2,20 @@
 
 printf("Started\n");
 
-#{
-function precision_color( percent )
-    switch percent
+function color = precision_color( percent )
+    if percent <= 25.0
+        color = [1, 0, 0.101];
+    elseif percent <= 50.0
+        color = [1, 0.6, 0.129];
+    elseif percent <= 75.0
+        color = [0.886, 0.933, 0.207];
+    elseif percent <= 100.0
+        color =  [0.282, 0.772, 0.207];
+    else
+        color = "w";
+    endif
 endfunction
-#}
+
 function csv_plot( file )
     db = csvread( file );
     
@@ -14,23 +23,30 @@ function csv_plot( file )
     alert = db( 1:end, 1 ) + 1.5*db( 1:end, 3 );
     alert_down = db( 1:end, 1 ) - 1.5*db( 1:end, 3 );
     arithimetic_mean_day = db( 1:end, 4 );
+    precision = db( 1:end, 5 );
 
     for i=2:1:length( db )
         
-        axis([ i-30 i+10 min(alert_down)-100 max(alert) + 50 ])
+        axis([ i-30 i+10 min(alert_down)-100 max(alert) + 50 ]);
+        pbaspect([2 1 1]);
 
-        
+
+        color = precision_color( precision(i) );
+
         if arithimetic_mean_day(i) == -1
             plot( [i-1, i], [arithimetic_mean_day(i-1), arithimetic_mean_day(i)], "color", "k", "linestyle", "none" );
         elseif arithimetic_mean_day(i-1) == -1
             plot( i, arithimetic_mean_day(i), "color", "b", "linewidth", 2 );
         else
-            plot( [i-1, i], [arithimetic_mean_day(i-1), arithimetic_mean_day(i)],"bo-","color", "b", "linewidth", 2, "markersize", 2.5 );
+            plot( [i-1, i], [arithimetic_mean_day(i-1), arithimetic_mean_day(i)],"bo-", "color", [0, 0.258, 0.615], "linewidth", 2, "markersize", 2.5, "markerfacecolor", color, "markeredgecolor", color );
         endif
-        plot( [i-1, i], [arithimetic_mean_28(i-1), arithimetic_mean_28(i)], "color", "r", "linewidth", 2 );
 
-        plot( [i-1, i], [alert(i-1), alert(i)], "color", "r", "linewidth", 1 );
-        plot( [i-1, i], [alert_down(i-1), alert_down(i)], "color", "r", "linewidth", 1 );
+
+        #set(gca,'color',[0.222, 0.214, 0.214])
+        plot( [i-1, i], [arithimetic_mean_28(i-1), arithimetic_mean_28(i)], "color", [1, 0, 0], "linewidth", 2 );
+
+        plot( [i-1, i], [alert(i-1), alert(i)], "color", [1, 0.337, 0.439], "linewidth", 1 );
+        plot( [i-1, i], [alert_down(i-1), alert_down(i)], "color", [1, 0.337, 0.439], "linewidth", 1 );
 
 
         hold on;
@@ -39,12 +55,15 @@ function csv_plot( file )
     endfor
 
     figure(1);
+    axis([0 i+10 min( alert_down )-100 max( alert )+50 ]);
+    pbaspect([3 1 1]);
+    #set(gca,'color',[0.222, 0.214, 0.214]);
     hold on;
 
-    #pause(30);
+    pause(10);
 
-    file_name = cstrcat( file, ".pdf" );
-    print ( 1, file_name );
+    #file_name = cstrcat( file, ".pdf" );
+    #print ( 1, file_name );
 
 endfunction
 
